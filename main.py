38d6cf76e7f87499676c25ceaa8f6977c07beca9
Michael Kahr <mikey.kahr@gmail.com>
2019-03-29 17:52:27 +0100

    implemented the "main" class

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import threading
 import queue
+from FileConsumer import FileConsumer
+from FileProducer import FileProducer
 
 myqueue = queue.Queue(maxsize=3)
 myCondition = threading.Condition()
+
+fileProd1 = FileProducer(name = "Producer 1",queue=myqueue,condition=myCondition)
+fileCons1 = FileConsumer(name = "Consumer 1",queue = myqueue,condition = myCondition)
+fileProd1.start()
+fileCons1.start()
+fileProd1.join()
+fileCons1.join()
