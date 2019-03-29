63b413161d60ac2055c71fd626246fc5f357a4d6
Michael Kahr <mikey.kahr@gmail.com>
2019-03-29 18:20:06 +0100

    created unit test file but no tests implemented yet

---
+++ test_project.py
@@ -0,0 +1,11 @@
+import unittest
+from pwdProducer import Password
+
+class test_Password(unittest.TestCase):
+
+    def test_check1(self):
+        #i dont know what to test...
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
